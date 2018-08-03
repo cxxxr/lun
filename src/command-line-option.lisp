faaf0659c576ae3f6953709db55fb0c8d6f3e03c
cxxxr <g23tlm@gmail.com>
2018-08-03 23:54:09 +0900

    prepare install command

--- src/command-line-option.lisp
+++ src/command-line-option.lisp
@@ -1,19 +1,28 @@
 (in-package :lun)
 
 (defstruct option
-  (forms '()))
+  (forms '())
+  (install))
+
+(defun read-arguments (option arguments)
+  (loop
+    :for arg := (pop arguments)
+    :do (cond ((member arg '("-e" "--eval") :test #'string=)
+               (push-end (list :eval (pop arguments)) (option-forms option)))
+              ((member arg '("-l" "--load") :test #'string=)
+               (push-end (list :load (pop arguments)) (option-forms option)))
+              ((member arg '("-s" "--system") :test #'string=)
+               (push-end (list :system (pop arguments)) (option-forms option))))
+    :while arguments)
+  arguments)
+
+(defun read-install-arguments (option arguments)
+  (declare (ignore option))
+  arguments)
 
 (defun process-command-line-arguments (arguments)
   (let ((option (make-option)))
-    (loop
-      :for arg := (pop arguments)
-      :do (cond ((member arg '("-e" "--eval") :test #'string=)
-                 (push-end (list :eval (pop arguments)) (option-forms option)))
-                ((member arg '("-l" "--load") :test #'string=)
-                 (push-end (list :load (pop arguments)) (option-forms option)))
-                ((member arg '("-s" "--system") :test #'string=)
-                 (push-end (list :system (pop arguments)) (option-forms option)))
-                ((member arg '("--install") :test #'string=)
-                 ))
-      :while arguments)
+    (when (equal "install" (first arguments))
+      (setf arguments (read-install-arguments option (rest arguments))))
+    (setf arguments (read-arguments option arguments))
     option))
